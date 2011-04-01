0bc03aa2f482ae654fc5b2444c4add72cb38c686
Guilherme Chapiewski <guilherme.chapiewski@gmail.com>
2011-03-31 21:43:01 -0300

    Added run, test and clean targets.

--- Makefile
+++ Makefile
@@ -1,3 +1,24 @@
-clean:
-	@rm -rf ./SampleApp/build/iphone/*
-	@echo "Cleaned."
+export PROJECT_NAME=SampleApp
+export PROJECT_ROOT=$(shell pwd)
+export TMP_DIR=$(PROJECT_ROOT)/tmp/
+export SVN_DIR=$(TMP_DIR)/$(PROJECT_NAME)_trunk/
+
+run:
+	@mkdir -p ${PROJECT_ROOT}/${PROJECT_NAME}/Resources/test/
+	@echo "" > ${PROJECT_ROOT}/${PROJECT_NAME}/Resources/test/enabled.js
+	@make launch-titanium
+
+test:
+	@mkdir -p ${PROJECT_ROOT}/${PROJECT_NAME}/Resources/test/
+	@echo "sampleapp.tests_enabled = true;" > ${PROJECT_ROOT}/${PROJECT_NAME}/Resources/test/enabled.js
+	@make launch-titanium
+
+clean: clean-languages
+	@rm -rf ${PROJECT_ROOT}/${PROJECT_NAME}/build/iphone/*
+	@mkdir -p ${PROJECT_ROOT}/${PROJECT_NAME}/build/iphone/
+	@echo "Deleted: ${PROJECT_ROOT}/${PROJECT_NAME}/build/iphone/*"
+
+launch-titanium:
+	@echo "Building with Titanium..."
+	@mkdir -p ${PROJECT_ROOT}/${PROJECT_NAME}/build/iphone/
+	@PROJECT_ROOT=${PROJECT_ROOT} bash ${PROJECT_ROOT}/bin/titanium.sh
---
+++ bin/titanium.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+PROJECT_NAME=${PROJECT_NAME}
+PROJECT_ROOT=${PROJECT_ROOT:-../}
+IPHONE_SDK_VERSION="4.2"
+TI_SDK_VERSION="1.6.1"
+TI_DIR="/Library/Application\ Support/Titanium"
+TI_ASSETS_DIR="${TI_DIR}/mobilesdk/osx/${TI_SDK_VERSION}"
+TI_IPHONE_DIR="${TI_ASSETS_DIR}/iphone"
+TI_BUILD="${TI_IPHONE_DIR}/builder.py"
+APP_DEVICE="iphone"
+
+if [ "PROJECT_NAME" == "" ]; then
+	echo "[ERROR] Please inform PROJECT_NAME."
+	exit 1
+fi
+
+# Get APP parameters from current tiapp.xml
+APP_ID=`cat $(PROJECT_NAME)/tiapp.xml | grep "<id>" | sed -e "s/<\/*id>//g"`
+APP_NAME=`cat $(PROJECT_NAME)/tiapp.xml | grep "<name>" | sed -e "s/<\/*name>//g"`
+
+if [ "APP_ID" == "" ] || [ "APP_NAME" == "" ]; then
+	echo "[ERROR] Could not obtain APP parameters from tiapp.xml file (does the file exist?)."
+	exit 1
+fi
+
+bash -c "${TI_BUILD} run ${PROJECT_ROOT}/${PROJECT_NAME}/ ${IPHONE_SDK_VERSION} ${APP_ID} ${APP_NAME} ${APP_DEVICE}" \
+	| perl -pe 's/^\[DEBUG\].*$/\e[35m$&\e[0m/g;s/^\[INFO\].*$/\e[36m$&\e[0m/g;s/^\[WARN\].*$/\e[33m$&\e[0m/g;s/^\[ERROR\].*$/\e[31m$&\e[0m/g;'
+
+killall "iPhone Simulator"
